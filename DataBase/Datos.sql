01edf6838ec7a3b88ea70682b5e67757980f6686
abrahamelfahsi <abrahamelfahsi@gmail.com>
2021-12-12 11:00:49 +0100

    Despligue Codigo

--- DataBase/Datos.sql
+++ DataBase/Datos.sql
@@ -18,10 +18,214 @@
 --
 -- Table structure for table `usuarios`
 --
+CREATE TABLE `comentario` (
+  `idComentario` int(11) NOT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  `contenido` varchar(150) DEFAULT NULL,
+  `idProducto` int(11) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comentario`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `idProducto` int(11) NOT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  `fechaIni` datetime DEFAULT NULL,
+  `fechaFin` datetime DEFAULT NULL,
+  `precioInicial` double DEFAULT NULL,
+  `idSeccion` int(11) DEFAULT NULL,
+  `proImagen` varchar(150) DEFAULT NULL,
+  `Descripcion` varchar(150) DEFAULT NULL,
+  `titulo` varchar(50) DEFAULT NULL,
+  `precioEnvio` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puja`
+--
+
+CREATE TABLE `puja` (
+  `idPuja` int(11) NOT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  `idProducto` int(11) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `valor` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `puja`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seccion`
+--
+
+CREATE TABLE `seccion` (
+  `idSeccion` int(11) NOT NULL,
+  `nombreSec` varchar(50) DEFAULT NULL,
+  `info` varchar(150) DEFAULT NULL,
+  `image` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `seccion`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  `dni` varchar(9) DEFAULT NULL,
+  `comunidad` varchar(50) DEFAULT NULL,
+  `provincia` varchar(50) DEFAULT NULL,
+  `cp` varchar(5) DEFAULT NULL,
+  `direccion` varchar(70) DEFAULT NULL,
+  `Rol` varchar(20) DEFAULT NULL,
+  `telefono` varchar(9) DEFAULT NULL,
+  `email` varchar(70) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`idComentario`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idProducto` (`idProducto`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`idProducto`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idSeccion` (`idSeccion`);
+
+--
+-- Indices de la tabla `puja`
+--
+ALTER TABLE `puja`
+  ADD PRIMARY KEY (`idPuja`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idProducto` (`idProducto`);
+
+--
+-- Indices de la tabla `seccion`
+--
+ALTER TABLE `seccion`
+  ADD PRIMARY KEY (`idSeccion`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `idComentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `puja`
+--
+ALTER TABLE `puja`
+  MODIFY `idPuja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `seccion`
+--
+ALTER TABLE `seccion`
+  MODIFY `idSeccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`);
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`idSeccion`) REFERENCES `seccion` (`idSeccion`);
+
+--
+-- Filtros para la tabla `puja`
+--
+ALTER TABLE `puja`
+  ADD CONSTRAINT `puja_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `puja_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`);
+COMMIT;
+
 INSERT INTO `comentario` (`idComentario`, `idUsuario`, `contenido`, `idProducto`, `fecha`) VALUES
 (1, 1, '', 3, '2021-12-07 11:53:52'),
 (2, 1, 'hola', 3, '2021-12-07 12:02:48'),
 (3, 1, 'jajajaja', 4, '2021-12-08 11:01:36');
+
 INSERT INTO `producto` (`idProducto`, `idUsuario`, `fechaIni`, `fechaFin`, `precioInicial`, `idSeccion`, `proImagen`, `Descripcion`, `titulo`, `precioEnvio`) VALUES
 (1, 1, NULL, NULL, 80.15, 1, NULL, 'Nueva', 'bicicleta', NULL),
 (2, 1, NULL, NULL, 80, 1, NULL, 'viejo', 'casco', NULL),
