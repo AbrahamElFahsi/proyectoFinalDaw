e9495c595f570295904cace09192f664ad98e9c9
abrahamelfahsi <abrahamelfahsi@gmail.com>
2021-12-13 19:56:43 +0100

    Despligue Codigo

--- Codigo/Conexion_BD.php
+++ Codigo/Conexion_BD.php
@@ -1,22 +1,17 @@
 <?php
         //Lo primero debemos incluir el fichero donde esta el conector
         require 'ConectorBD.php';
+        require 'DAOUsuario.php';
         echo "Se ha cargado correctamente el archivo <br>";
 
         //Queremos conectarnos con la BD. 
         $conexion = conectar(false);
         $consulta = "SELECT * FROM usuario";
 
-if (!$resultado = $conexion->query($consulta)) {
-    echo "Lo sentimos, no se pudo realizar la consulta.";
-    exit;
-}
-
-echo "<table><tr><th>Usuario</th><th>Clave</th></tr>";
+$resultado=todosUsuarios($conexion);
 while ($array_registro = $resultado->fetch_assoc()) {
     echo $array_registro['idUsuario']." ".$array_registro['nombre']."<br>";
 }
-echo "</table>";
 
 $resultado->free(); 
 $conexion->close();
